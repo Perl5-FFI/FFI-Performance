c36828f49fd9e134f547eff2d43bbb71accfb46d
Graham Ollis <plicease@cpan.org>
2014-10-23 13:12:28 -0400

    corrections

---
+++ lib/FFI/Performance/Raw.pm
@@ -0,0 +1,45 @@
+package FFI::Performance::Raw;
+
+use strict;
+use warnings;
+use FFI::Raw;
+use base qw( Exporter );
+
+our @EXPORT = qw( test1 test2 test3 );
+
+my $lib = 'foo/libfoo.so';
+
+die "no such library $lib" unless -e $lib;
+
+my $test1 = FFI::Raw->new(
+  $lib, 'test1',
+  FFI::Raw::int,
+  FFI::Raw::int,
+);
+
+my $test2 = FFI::Raw->new(
+  $lib, 'test2',
+  FFI::Raw::str,
+  FFI::Raw::str,
+);
+
+my $test3 = FFI::Raw->new(
+  $lib, 'test3',
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+  FFI::Raw::int,
+);
+
+sub test1 { $test1->call(@_) }
+sub test2 { $test2->call(@_) }
+sub test3 { $test3->call(@_) }
+
+1;
--- t/use.t
+++ t/use.t
@@ -1,5 +1,6 @@
 use strict;
 use warnings;
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 use_ok 'FFI::Performance';
+use_ok 'FFI::Performance::Raw';
