f93cbcdc18a1db7eeaa97ef28e86b6d215775cda
Graham Ollis <perl@wdlabs.com>
2015-01-18 16:15:05 -0500

    updates for new versions...

--- lib/FFI/Performance/Platypus.pm
+++ lib/FFI/Performance/Platypus.pm
@@ -14,8 +14,8 @@ die "no such library $lib" unless -e $lib;
   
 lib $lib;
 
-function test1 => [int] => int;
-function test2 => [string] => string;
-function test3 => [int,int,int,int,int,int,int,int,int,int] => int; 
+attach test1 => [int] => int;
+attach test2 => [string] => string;
+attach test3 => [int,int,int,int,int,int,int,int,int,int] => int; 
 
 1;
